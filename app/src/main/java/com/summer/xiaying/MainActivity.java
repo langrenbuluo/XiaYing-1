1ceaad6186faa757a2635aa4bdd7efbcd6ac6102
fengdonglin <1281031295@qq.com>
2016-09-30 15:08:18 +0800

    start页面

---
+++ app/src/main/java/com/summer/xiaying/MainActivity.java
@@ -0,0 +1,40 @@
+package com.summer.xiaying;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.summer.xiaying.activity.guideActivity;
+import com.summer.xiaying.activity.work_Activity;
+
+public class MainActivity extends AppCompatActivity {
+    Intent it=null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        init();
+    }
+    private void init() {
+        SharedPreferences sp=getSharedPreferences("args", Context.MODE_PRIVATE);
+        boolean isShow=sp.getBoolean("isShow", false);
+        if (isShow) {
+            it=new Intent(this,work_Activity.class);
+        }else {
+            it=new Intent(this,guideActivity.class);
+        }
+        new Thread(){
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                    startActivity(it);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+}
